bff977214ef0942d86a0b7a058c0c1e1f7fb7808
wxy20850606 <98997211+wxy20850606@users.noreply.github.com>
2022-12-29 11:58:50 +0800

    Delete lab10 directory

--- lab10/lab10.scm
+++
@@ -1,9 +0,0 @@
-(define lst 
-(list((list 1) 2 list(3 4) list(5)))
-(define lst1
-  (cons (cons 1 nil)
-        (cons 2
-              (cons (cons 3 (cons 4 nil))
-                    (cons 5 nil))))
-)
-)
